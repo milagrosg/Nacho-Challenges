2a857546dd1d6f79fd23aafae88bee70f0cb005f
Mili <milagrosg@gmail.com>
2014-05-30 13:11:37 -0400

    Getting 10 tweets from timeline with username, date created and content and then saving it into a CSV file.

---
+++ Challenge_1/get_my_tweets2.rb
@@ -0,0 +1,34 @@
+require "rubygems"
+require "twitter"
+require "csv"
+
+# Twitter API config
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key = "ypZTEZ6JQybfl0SZfRRRBsVVu"
+	config.consumer_secret = "Cmr6Amhxn1OVCV9TlgWecFRrO4BdgScarTpFz35ikGM6xOXEci"
+	config.access_token = "54199181-7KtIh2B8dJTt3ZEl8eYbtr78QhM1Wd95ELCpOJdGp"
+	config.access_token_secret = "Rj7xJGTkyDSH2OmGr46w4WyUKYXIhK0KnxthP5jJ3DFGj"
+end
+
+# Getting tweets from my timeline
+last_tweets = client.home_timeline(options = {:count => 10})
+
+# Printing tweets
+puts "This is the list of tweets:"
+
+last_tweets.each do |tweet|
+	puts "Username: #{tweet.user.name}"
+	puts "Tweet: #{tweet.text}"
+	puts "Date: #{tweet.created_at}\n"
+end
+
+# Writing tweets to a CSV file. One entry per each line.  
+CSV.open("/Users/Mili/Documents/LearningRuby/Nacho_Challenges/Challenge_1/tweets.csv", "wb") do |csv|
+	last_tweets.each do |tweet|
+	csv << ["Username: #{tweet.user.name}", "Tweet: #{tweet.text}", "Date: #{tweet.created_at}"]	
+	end
+end
+
+# Closing file
+
+
