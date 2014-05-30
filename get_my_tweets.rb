d8e1635b88f7911184494f4386daa15ad143a59a
Mili <milagrosg@gmail.com>
2014-05-30 11:47:31 -0400

    Showing 20 tweets and it's creation date and saving it to a file.

--- get_my_tweets.rb
+++ get_my_tweets.rb
@@ -9,17 +9,26 @@ client = Twitter::REST::Client.new do |config|
 	config.access_token_secret = "Rj7xJGTkyDSH2OmGr46w4WyUKYXIhK0KnxthP5jJ3DFGj"
 end
 
-# Getting first tweet from my timeline
-last_tweets = client.home_timeline.first.text
+# Getting tweets from my timeline
+last_tweets = client.home_timeline
 
-# Printing tweet
-puts "This is the first tweets: #{last_tweets}"
+# Printing tweets
+puts "This is the list of tweets:"
+
+last_tweets.each do |tweet|
+	puts "#{tweet.created_at}"
+	puts "#{tweet.text}\n"
+end
 
 # Creating / Opening file in write mode
 target = File.open("tweets.txt", 'w')
 
 # Writing tweets to file  
-target.write(last_tweets)
+last_tweets.each do |tweet|
+	target.write(tweet.created_at)
+	target.write(tweet.text)
+	target.write("\n")
+end
 
 # Closing file
 target.close()
