018d7464c1f825598cd8d410ff09fc984ec235c9
Mili <milagrosg@gmail.com>
2014-06-08 18:36:41 -0400

    Added logic to display my last 25 tweets. Still having some trouble with haml :)

--- Challenge_2/my_tweets.rb
+++ Challenge_2/my_tweets.rb
@@ -3,10 +3,20 @@ require "cuba/render"
 
 Bundler.require
 
+Dotenv.load 
+
 Cuba.use Rack::Session::Cookie
 
 Cuba.plugin Cuba::Render
 Cuba.settings[:render][:template_engine] = "haml"
+
+#Twitter API setup
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+	config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+	config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+	config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
 
 Cuba.define do 
 	
@@ -16,8 +26,13 @@ Cuba.define do
 			res.redirect "/my_tweets"
 		end
 
-		on "my_tweets" do 
-			res.write render("views/list_tweets.haml", content: "my tweets")
+		on "my_tweets" do
+			last_tweets = client.home_timeline(options = {:count => 25})
+
+			last_tweets.each do |tweet|
+				res.write render("views/list_tweets.haml", content: "#{tweet.created_at}")
+				res.write render("views/list_tweets.haml", content: "#{tweet.text}\n")
+			end 
 		end
 
 		on "about" do 
