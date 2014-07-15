ad6a3bb1dc4f9b6421492abde16a74d11102a025
Mili <milagrosg@gmail.com>
2014-07-15 11:19:02 -0400

    Reading from a file

--- challenge_3/ch_3.rb
+++ challenge_3/ch_3.rb
@@ -1,9 +1,15 @@
 require './histogram'
 
-puts "Input your text for word count: "
-input = gets.chomp
+filename = ARGV.first
 
-count = Histogram.new(input)
+while filename == nil || filename == "" do
+  puts "No filename specified. Please try again!"
+  filename = STDIN.gets.chomp()
+end 
+
+txt = File.open(filename)
+
+count = Histogram.new(txt.read)
 
 puts count.data
 puts count.by_first_letter
